9e74102a2303a47010ac58151599bde845ec064c
ttbangg <qtaibat@yahoo.com>
2026-02-26 10:29:24 +0000

    Update NHS_DNA_Analysis.sql

--- NHS_DNA_Analysis.sql
+++ NHS_DNA_Analysis.sql
@@ -14,7 +14,22 @@ UPDATE Appointments SET IMD_Quintile = 1 WHERE IMD_Quintile IS NULL; -- Conserva
 ALTER TABLE Appointments ADD COLUMN DNA_Flag INT;
 UPDATE Appointments SET DNA_Flag = CASE WHEN Attended = 'No' THEN 1 ELSE 0 END;
 
--- 3. STRATEGIC INSIGHT QUERY
+-- 3. OVERALLL DNA RATE (All Cohorts)
+SELECT 
+    COUNT(*) AS Total_Appointments,
+    SUM(CASE WHEN Attended = 'No' THEN 1 ELSE 0 END) AS Total_Missed,
+    ROUND(AVG(CASE WHEN Attended = 'No' THEN 1.0 ELSE 0 END) * 100, 1) AS DNA_Rate_Percent
+FROM Appointments;
+
+-- 4. RELATIVE RISK CALCULATION
+SELECT 
+    IMD_Quintile,
+    ROUND(AVG(DNA_Flag) * 100, 1) AS DNA_Rate_Percent
+FROM Appointments
+GROUP BY IMD_Quintile
+ORDER BY IMD_Quintile;
+
+-- 5. STRATEGIC INSIGHT QUERY
 -- Calculating total loss and DNA rates for the high-priority group (IMD 1 & 2)
 SELECT 
     GP_Practice,
@@ -25,3 +40,4 @@ FROM Appointments
 WHERE IMD_Quintile IN (1, 2)
 GROUP BY GP_Practice
 ORDER BY Total_Missed_Appts DESC;
+
